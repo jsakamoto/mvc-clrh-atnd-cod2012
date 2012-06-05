06f134b5a3af94078c82e5d506e38f5615495efa
jsakamoto <junichi.jp@gmail.com>
2012-06-05 19:58:38 +0900

    Sample web app for Community Open Day 2012, Hokkaido, Session-2.

---
+++ atnd/Controllers/AtndController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using atnd.Models;
+
+namespace atnd.Controllers
+{
+    public class AtndController : Controller
+    {
+        private AtndDB db = new AtndDB();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View(db.Attendees.ToList());
+        }
+
+        /// <summary>
+        /// Regist
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Regist()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Regist(Attendee attendee, string cmd)
+        {
+            if (cmd == "Cancel") return RedirectToAction("Index");
+
+            if (ModelState.IsValid)
+            {
+                db.Attendees.Add(attendee);
+                db.SaveChanges();
+
+                return RedirectToAction("Complete");
+            }
+
+            return View(attendee);
+        }
+
+        public ActionResult Complete()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ atnd/Models/Attendee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace atnd.Models
+{
+    public class Attendee
+    {
+        public int AttendeeID { get; set; }
+
+        [Required, StringLength(20)]
+        public string Name { get; set; }
+
+        [Required, StringLength(20), Display(Name="e-mail")]
+        public string Email { get; set; }
+
+        [StringLength(30)]
+        public string Comment { get; set; }
+    }
+}
